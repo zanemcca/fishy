a0974f24177a91b74644ed8a2db96744b1d1ef24
Zane McCaig <zane@instanews.com>
2016-12-29 15:47:23 -0800

    Added tensorboard summaries for fishy_eval

--- fishy_eval.py
+++ fishy_eval.py
@@ -6,10 +6,9 @@ import tensorflow as tf
 import fishy
 import numpy as np
 import fishy_input
+import fishy_constants as const
 
-
-FLAGS = tf.app.flags.FLAGS
-CATEGORIES = fishy_input.CATEGORIES
+FLAGS = const.FLAGS
 
 def evaluate(set_type='Test'):
   print('Starting the session')
@@ -17,11 +16,21 @@ def evaluate(set_type='Test'):
   with tf.Graph().as_default():
     images, labels = fishy.inputs(set_type)
     num_examples_per_epoch = fishy_input.get_input_length(os.getcwd(),set_type)
-    logits = fishy.inference(images)
+    predictions,_ = fishy.inference(images)
 
-    predictions = tf.nn.softmax(logits)
+    #lss = tf.contrib.losses.log_loss(predictions, tf.one_hot(labels, const.NUM_CLASSES), tf.truediv(1, FLAGS.batch_size), 1e-15) 
+    lss = tf.contrib.losses.log_loss(predictions, tf.one_hot(labels, const.NUM_CLASSES), 1, 1e-15) 
+
+    correct_prediction = tf.equal(tf.cast(labels, tf.int64), tf.argmax(predictions, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) 
 
     saver = tf.train.Saver()
+
+    writer = tf.summary.FileWriter(FLAGS.log_dir, graph=tf.get_default_graph())
+    tf.summary.scalar('Evaluation Cost', lss)
+    tf.summary.scalar('Evaluation Accuracy', accuracy)
+
+    summary = tf.summary.merge_all()
 
     with tf.Session() as s:
       saver.restore(s, "/tmp/fishy_model.ckpt")
@@ -37,7 +46,14 @@ def evaluate(set_type='Test'):
           correct = 0
           while not coord.should_stop():
               step += 1
-              probs= predictions.eval()
+              (loss, acc) = s.run([lss, accuracy])
+
+              summ = s.run(summary)
+
+              writer.add_summary(summ, step * FLAGS.batch_size)
+
+              """
+              probs = predictions.eval()
               Y = labels.eval()
 
               cor = 0
@@ -46,7 +62,7 @@ def evaluate(set_type='Test'):
                 if(top == y):
                   cor += 1
 
-                topChoices = [CATEGORIES[idx] for idx in np.argsort(probs[i])]
+                topChoices = [const.CLASSES[idx] for idx in np.argsort(probs[i])]
                 topChoices.reverse()
                 predic = ''
                 for c in topChoices:
@@ -54,14 +70,20 @@ def evaluate(set_type='Test'):
                 #print([round(p, 5) for p in probs[i]])
                 prob = probs[i][y]
                 cost = -math.log(max(min(prob, 1),1e-15))
-                #print('(y, p): (' + CATEGORIES[y] + ', ' + predic + ')\tP(y): ' + str(round(prob, 2)) + '\tCost: ' + str(round(cost,2)))
+                #print('(y, p): (' + const.CLASSES[y] + ', ' + predic + ')\tP(y): ' + str(round(prob, 2)) + '\tCost: ' + str(round(cost,2)))
                 total = total + cost 
 
-              print(str(step) + '\tPercentCorrect = ' + str(cor * 100 / FLAGS.batch_size) + '\tCost = ' +  str(total / (step * FLAGS.batch_size)))
+              print(str(step) + '\tAccuracy = ' + str(round(acc, 3)) + '\tPercentCorrect = ' + str(cor * 100 / FLAGS.batch_size) + '\tLoss = ' + str(loss) + '\tCost = ' +  str(total / (step * FLAGS.batch_size)))
               correct += cor
+              """
+              print(str(step) + '\tAccuracy = ' + str(round(acc, 3))+ '\tLoss = ' + str(loss))
+
+              total += loss
+              correct += acc
+
               if step % steps_per_epoch == 0:
-                print('\nThe final Cost = ' + str(round(total / num_examples_per_epoch, 2)) + ' with a ' + str(correct * 100 / num_examples_per_epoch) + '% of predictions being successful')
-                return total / num_examples_per_epoch
+                print('\nThe final Cost = ' + str(round(total / steps_per_epoch, 3)) + ' with a ' + str(round(correct * 100 / steps_per_epoch)) + '% of predictions being successful')
+                return correct / steps_per_epoch 
 
       except tf.errors.OutOfRangeError:
           print('out of range evaluate')
