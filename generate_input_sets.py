2b02d20fafe0e47806f7cd22f1cc80dfabca8f16
Zane McCaig <zane@instanews.com>
2016-12-31 15:36:32 -0800

    Changed the input generator to use relative paths

--- generate_input_sets.py
+++ generate_input_sets.py
@@ -1,28 +1,39 @@
 
 import os
+import glob
 import csv
 import random
 
 CATEGORY_NAMES = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
 
-def find_input_pairs(data_dir):
+def find_input_pairs(data_dir, label_data=True):
   filenames = []
-  for (dirpath, dirnames, files) in os.walk(data_dir):
-    filenames.extend([os.path.join(dirpath, f) for f in files if ('.jpg' in f or '.jpeg' in f)])
+
+  #for (dirpath, dirnames, files) in os.walk(data_dir):
+  #  filenames.extend([os.path.join(dirpath, f) for f in files if ('.jpg' in f or '.jpeg' in f)])
+
+  filenames.extend([f for f in glob.iglob(os.path.join(data_dir, '*.jpg'))])
+  filenames.extend([f for f in glob.iglob(os.path.join(data_dir, '**', '*.jpg'))])
+  filenames.extend([f for f in glob.iglob(os.path.join(data_dir, '*.jpeg'))])
+  filenames.extend([f for f in glob.iglob(os.path.join(data_dir, '**', '*.jpeg'))])
 
   io_pairs = [] 
   for f in filenames:
-    label =-1 
-    for i, c in enumerate(CATEGORY_NAMES):
-      if c in f:
-        label = i
-        break
+    if(label_data):
+      label =-1 
+      for i, c in enumerate(CATEGORY_NAMES):
+        if c in f:
+          label = i
+          break
 
-    if(label >= 0):
-      pair = { 'filename': f, 'label': label }
-      io_pairs.append(pair)
+      if(label >= 0):
+        pair = { 'filename': f, 'label': label }
+        io_pairs.append(pair)
+      else:
+        raise ValueError('No label found for ' + f)
     else:
-      raise ValueError('No label found for ' + f)
+      pair = { 'filename': f, 'label': 0 }
+      io_pairs.append(pair)
 
   return io_pairs
 
@@ -31,10 +42,8 @@ def split_set(io_pairs, percent_test=20.0, percent_cv=20.0):
   cv_size = int(percent_cv * len(io_pairs) / 100)
   test_size = int(percent_test * len(io_pairs) / 100)
 
-  print(len(io_pairs))
+  io_pairs = io_pairs[:]
   random.shuffle(io_pairs)
-  print(len(io_pairs))
-  print(cv_size)
   cv = io_pairs[-cv_size:] 
   del io_pairs[-cv_size:]
 
@@ -46,8 +55,8 @@ def split_set(io_pairs, percent_test=20.0, percent_cv=20.0):
   return train, test, cv
 
 
-def get_sets(data_dir, percent_test=20.0, percent_cv=20.0):
-  io_pairs = find_input_pairs(data_dir)
+def get_sets(data_dir, percent_test=20.0, percent_cv=20.0, label_data=True):
+  io_pairs = find_input_pairs(data_dir, label_data)
 
   return split_set(io_pairs, percent_test, percent_cv)
 
@@ -69,14 +78,18 @@ def read_set(filename):
 
 
 def main(argv=None):
-  # TODO Get percent_test and percent_cv from input arguments
   percent_test = 20.0
   percent_cv = 20.0
-  (train, test, cv) = get_sets(os.path.join(os.getcwd(), 'train'), percent_test, percent_cv)
 
+  data = find_input_pairs('train')
+  (train, test, cv) = split_set(data, percent_test, percent_cv)
+
+  submission_input = find_input_pairs('test_stg1', False)
+  write_set(data, 'all.csv')
   write_set(train, 'train.csv')
   write_set(test, 'test.csv')
   write_set(cv, 'cv.csv')
+  write_set(submission_input, 'submission_input.csv')
 
 if __name__ == '__main__':
   main()
